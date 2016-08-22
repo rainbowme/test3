481f01b18986ca63e4b3b796808930e69988a48a
Aurora <aurora@JohndeMacBook-Pro.local>
2016-08-22 18:05:41 +0800

    initproject

---
+++ asdfghj/asdfghj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  asdfghj
+//
+//  Created by Aurora on 16/8/22.
+//  Copyright © 2016年 howbuy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ asdfghj/asdfghj/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  asdfghj
+//
+//  Created by Aurora on 16/8/22.
+//  Copyright © 2016年 howbuy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
